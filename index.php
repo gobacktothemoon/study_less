63cde941247d47c1744edce49ef2952628749ede
jyp <hp5959@naver.com>
2017-04-23 20:22:14 +0900

    edit .gitignore file

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-<?php
-	echo 11+22;
+<html>
+<head>
+<style>
+#jyp{
+	background-color: blue;
+	color: white;
+}
+</style>
+</head>
+<body>
+<div id='jyp'>
+	<img src='nono.jpg'/>where am i<?=3+8?>
+</div>
+</body>
+</html>
