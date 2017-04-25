4c70e8a17b288dca00320902c34cc395ee582772
jyp <hp5959@naver.com>
2017-04-25 19:03:33 +0900

    good job

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <html>
 <head>
-<link rel='stylesheet' href="./css/modular.css"/> 
+<link rel='stylesheet' href="./css/jyp.css"/> 
 </head>
 <body>
 
 <div class='header'>
 	<div class='container'>
-		우리는 민족중흥의 역사적 사명을 띄고 이땅에 태어났다.
+		우리는 민족중흥의 역사적 사wefwefewf명을 띄고 이땅에 태어났다.
+	</div>
+</div>
+<div class='contents'>
+	<div class='container'>
+		<div class='row'>
+			<div class='col-sm-3' style='background-color: yellow'>왼wefwe쪽</div>
+			<div class='col-sm-9' style='background-color: blue'>2fwef3wefwef왼쪽</div>
+		</div>
+	</div>
+</div>
+<div class='footer'>
+	<div class='container'>
+		footer
 	</div>
 </div>
 
