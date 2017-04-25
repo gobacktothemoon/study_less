246d17b616b53557565ce26c7d404c08f5cddfb8
jyp <hp5959@naver.com>
2017-04-25 17:09:37 +0900

    forgood

--- index.php
+++ index.php
@@ -3,8 +3,12 @@
 <link rel='stylesheet' href="./css/modular.css"/> 
 </head>
 <body>
-<div style='width: 100px;border:solid 14px #ccc'>
-ewfwefwef
+
+<div class='header'>
+	<div class='container'>
+		우리는 민족중흥의 역사적 사명을 띄고 이땅에 태어났다.
+	</div>
 </div>
+
 </body>
 </html>
